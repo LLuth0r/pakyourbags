5367c144bded19e30ac69d35114d36e716662508
Thomas Sten <thomas.e.sten@gmail.com>
2021-03-17 12:45:54 -0400

    local backend debugging

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://pakyourbags.herokuapp.com/';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'heroku-deploy-link' : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl
